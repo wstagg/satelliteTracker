7499e46e2e9c960e91263d7db67c1eff0fc308c9
Wesley Stagg <wes.stagg@gmail.com>
2025-11-06 21:33:28 +0000

    single satellite tracking

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '../../cpp/n2yoSatelliteApi/Release')
+sys.path.insert(1, '../../cpp/n2yo-satellite-api/cmake-build-release')
 import n2yoSatelliteApi
 
 import matplotlib
@@ -16,51 +16,61 @@ ax = fig.add_subplot(111)
 
 # Create basemap
 m = Basemap(projection='robin', lon_0=0, resolution='c', ax=ax)
-m.drawcoastlines()
-m.fillcontinents(color='coral', lake_color='aqua')
-m.drawparallels(np.arange(-90., 120., 30.))
-m.drawmeridians(np.arange(0., 360., 60.))
-m.drawmapboundary(fill_color='aqua')
+#m.drawcoastlines()
+#m.fillcontinents(color='coral', lake_color='aqua')
+#m.drawparallels(np.arange(-90., 120., 30.))
+#m.drawmeridians(np.arange(0., 360., 60.))
+#m.drawmapboundary(fill_color='aqua')
+m.bluemarble()
+m.drawcountries()
+
 
 config = n2yoSatelliteApi.Config()
 
-ok = config.read("../../cpp/n2yoSatelliteApi/config.txt")
+ok = config.read("../../cpp/n2yo-satellite-api/config.txt")
 
 if ok:
 
     dataReceiver = n2yoSatelliteApi.DataReceiver(config)
+    tle = dataReceiver.getTle()
 
-    sat_plots = []
+    satName = tle.satName
+    sat_data = []
+    sat_positions = []
+    sat_positions_to_clear = []
+    sat_data.append(sat_positions)
+    sat_data.append(sat_positions_to_clear)
 
-    def init():
-        return[]
-    def update(frame):
+    
+    def update(frame):            
+        # if no satellite positions left to plot, get more
+        if len(sat_data[0]) == 0:
+            satellitePosition = dataReceiver.getSatellitePosition()
+            sat_data[0] = satellitePosition.positionData
+            print(f"num of positions {len(sat_data[0])}")
 
-        # Clear previous satellite markers
-        for plot in sat_plots:
-            plot.remove()
+        # remove the last drawn plot
+        if len(sat_data[1]) > 0:
+            for plot in sat_data[1]:
+                plot[0].remove()
+            del sat_data[1][0]
+
+        # create the plot for the satellite position
+        xpt, ypt = m(sat_data[0][0].lon, sat_data[0][0].lat)
+        # delete the position from the positions to plot
+        del sat_data[0][0]
+
+        plot = m.plot(xpt, ypt, 'ro', markersize = 6)
+        sat_data[1].append(plot) 
+
             
-        sat_plots.clear()
-        
-        # Get and plot new positions
-        satsAbove = dataReceiver.getSatellitesAbove()
-        print(satsAbove.transactionCount)
-        for sat in satsAbove.satellitesAbove:
-            xpt, ypt = m(sat.lon, sat.lat)
-            plot, = m.plot(xpt, ypt, 'bo', markersize = 4)
-            sat_plots.append(plot)
-            print(sat.satName)
-        print(f"transaction count = {satsAbove.transactionCount}")
-        
-        return sat_plots
-
-    plt.title("Satellite Tracker")
+    plt.title(satName + " TRACKER")
 
     ani = animation.FuncAnimation(
     fig,
     update,
-    init_func=init,
-    interval=36000,
-    blit=True)
+    # init_func=init,
+    interval=1000,
+    blit=False)
 
     plt.show()
